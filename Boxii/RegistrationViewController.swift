6a86be0a0b646ead7f9bbcae719ca9917281ac57
Chen Yu Tsen <chenyutsen@c-73-186-158-202.hsd1.ma.comcast.net>
2014-06-18 23:34:13 -0400

    got it mostly working

--- Boxii/RegistrationViewController.swift
+++ Boxii/RegistrationViewController.swift
@@ -10,8 +10,6 @@ import UIKit
 
 
 class RegistrationViewController: UIViewController, UITextFieldDelegate {
-
-
   
     @IBOutlet var phoneNumberInput : UITextField = nil
     @IBOutlet var emailInput : UITextField = nil
@@ -19,6 +17,7 @@ class RegistrationViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet var scrollView : UIScrollView = nil
     var activeField : UITextField?
     var authenticationModel : AuthenticationModel?
+    
     init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
         // Custom initialization
@@ -32,7 +31,6 @@ class RegistrationViewController: UIViewController, UITextFieldDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         self.phoneNumberInput.delegate = self
         self.emailInput.delegate = self
         self.passwordInput.delegate = self
@@ -81,7 +79,7 @@ class RegistrationViewController: UIViewController, UITextFieldDelegate {
         var contentInsets = UIEdgeInsetsZero
         var testInsets = UIEdgeInsetsMake(500, 500, 500, 500)
         scrollView.setContentOffset(CGPointMake(0, 0), animated: true)
-        //scrollView.scrollIndicatorInsets = testInsets
+        scrollView.scrollIndicatorInsets = testInsets
     }
 
     
@@ -98,7 +96,8 @@ class RegistrationViewController: UIViewController, UITextFieldDelegate {
     func textField(textField: UITextField!, shouldChangeCharactersInRange range: NSRange, replacementString string: String!) -> Bool{
         var newLength = (textField.text as NSString).length + (string as NSString).length - range.length
         if newLength > 10 && (textField == self.phoneNumberInput){
-           
+            return false
+        }
         return true
     }
     
@@ -118,43 +117,43 @@ class RegistrationViewController: UIViewController, UITextFieldDelegate {
             NSNotificationCenter.defaultCenter().addObserver(self, selector: "loginSuccess:", name: NOTIFICATION_LOGIN_SUCCESS, object: nil)
 
     }
-    /*
-*/
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+        
     
-    override func viewWillDisappear(animated: Bool) {
-        NSNotificationCenter.defaultCenter().removeObserver(self)
-    }
-
-    
-    @IBAction func signupButtonPressed(sender : AnyObject) {
-        SVProgressHUD.show()
-        self.authenticationModel = AuthenticationModel(email: emailInput.text, phoneNumber: self.phoneNumberInput.text, password: passwordInput.text)
-        self.authenticationModel!.register()
-    }
-
-    /*Login selector to be called only after registration has succeeded*/
-    func loginFromRegistration(sender:AnyObject!){
-        if self.authenticationModel{
-            self.authenticationModel!.login()
+        override func didReceiveMemoryWarning() {
+            super.didReceiveMemoryWarning()
+            // Dispose of any resources that can be recreated.
         }
-    }
-
-    func loginSuccess(sender:AnyObject!){
-        SVProgressHUD.dismissHUDAsync()
-        self.performSegueWithIdentifier(SEGUE_REGISTRATION_TO_PROFILE, sender: self)
-    }
-    /*
-    // #pragma mark - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue?, sender: AnyObject?) {
+        
+        override func viewWillDisappear(animated: Bool) {
+            NSNotificationCenter.defaultCenter().removeObserver(self)
+        }
+        
+        
+        @IBAction func signupButtonPressed(sender : AnyObject) {
+            SVProgressHUD.show()
+            self.authenticationModel = AuthenticationModel(email: emailInput.text, phoneNumber: self.phoneNumberInput.text, password: passwordInput.text)
+            self.authenticationModel!.register()
+        }
+        
+        /*Login selector to be called only after registration has succeeded*/
+        func loginFromRegistration(sender:AnyObject!){
+            if self.authenticationModel{
+                self.authenticationModel!.login()
+            }
+        }
+        
+        func loginSuccess(sender:AnyObject!){
+            SVProgressHUD.dismissHUDAsync()
+            self.performSegueWithIdentifier(SEGUE_REGISTRATION_TO_PROFILE, sender: self)
+        }
+        /*
+        // #pragma mark - Navigation
+        
+        // In a storyboard-based application, you will often want to do a little preparation before navigation
+        override func prepareForSegue(segue: UIStoryboardSegue?, sender: AnyObject?) {
         // Get the new view controller using [segue destinationViewController].
         // Pass the selected object to the new view controller.
-    }
-    */
-
+        }
+        */
+    
 }
